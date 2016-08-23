c5e2850424022f678289c612266c71235faeb76c
Carlo Napolitano <carlonapolitano99@gmail.com>
2016-08-22 22:22:00 -0400

    Added more jQuery to the website to make it look interactive. Subscribe button makes an email form show up that when filled out and submitted, an alert pops up saying you are subscribed. Also, I added a loading icon to make the website look unique and fun as the user has to wait for their webpage to load. The icon is a gif that shows and hides for a period of time when a button, link, or tab is selected. Now that the foundation for all the websites is complete, start working on the secondary webpages where more info will be located.

--- jsfolder/main.js
+++ jsfolder/main.js
@@ -4,7 +4,53 @@ $(document).ready(function(){
         };
         $('#banner').slideUp(animate);
         
+          var loadpage=function(){
+          $('#loading').delay(2000).hide('fast');
+          };
+          $('#loading').show(loadpage);
+          
+          var hide_tabs_initial_load=function(){
+            $('#tabs').delay(2100).show('fast');
+          };
+            $('#tabs').hide(hide_tabs_initial_load);
+
         
+        $('#welcome').click(function(){
+           function hide(){
+          $('#tabs').delay(2000).show(750);
+          $('#loading').delay(1700).hide('fast');
+        }
+        $('#tabs').hide(hide);
+        $('#loading').show();
+        });
+        
+         $('#latestnews').click(function(){
+           function hide(){
+          $('#tabs').delay(2000).show(750);
+          $('#loading').delay(1700).hide('fast');
+        }
+        $('#tabs').hide(hide);
+        $('#loading').show();
+        });
+        
+         $('#summary').click(function(){
+           function hide(){
+          $('#tabs').delay(2000).show(750);
+           $('#loading').delay(1700).hide('fast');
+        }
+        $('#tabs').hide(hide);
+        $('#loading').show();
+        });
+       
+        $('.showsubscribeform').click(function(){
+          $(".subscribediv").hide();
+          $("#EmailForm").show();
+         });
+        $("#submit").click(function(){
+          $("#EmailForm").hide();
+          $(".subscribediv").show();
+          $('#loading').show();
+         });
           
           
       });
