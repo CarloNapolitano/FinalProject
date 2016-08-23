f3a0ccf1b1218206640aa7db73793e59dce1b2b0
Carlo <Carlo@new-host-2.home>
2016-08-22 22:02:12 -0400

    First page for the website of the final project is finished. Contains a jQuery UI TABS widget and has basic jQuery for the nav bar. All images and pictures are added and the links should be working. The tab for latest news needs to have a finished subscribe button with animations and maybe I should add a new animation to make the website look cool.

---
+++ jsfolder/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+      var animate=function(){
+         $('#banner').slideDown('slow');
+        };
+        $('#banner').slideUp(animate);
+        
+        
+          
+          
+      });
